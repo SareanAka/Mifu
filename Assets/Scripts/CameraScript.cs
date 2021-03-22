2c636bbeb4365d8e29e8ff0c9df21ba2f5995c77
VERMEIT3 <thomas.vermeij2@hva.nl>
2021-03-22 23:50:22 +0100

    Initial commit

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    /*private float panningCam;
+    private float camSpeed;
+    private bool camPause = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CameraPan());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (camPause)
+        {
+            camSpeed = 4 / Screen.width;
+            panningCam += camSpeed;
+
+            this.transform.position = new Vector3(panningCam, this.transform.position.y, this.transform.position.z);
+        }
+        
+    }
+
+    IEnumerator CameraPan()
+    {
+        yield return new WaitForSeconds(5);
+        camPause = true;
+    }*/
+}
