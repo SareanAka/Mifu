037ac3c6c9e1caf3bb26d1ecccca4e3b1930e876
VERMEIT3 <thomas.vermeij2@hva.nl>
2021-03-23 22:54:24 +0100

    Added second level and now it tells you which level you are in

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] Canvas canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Load());
+    }
+
+    // Update is called once per frame
+    IEnumerator Load()
+    {
+        yield return new WaitForSeconds(2);
+        canvas.enabled = false;
+    }
+}
--- Assets/Scripts/NextLevelScript.cs
+++ Assets/Scripts/NextLevelScript.cs
@@ -3,21 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class WinScript : MonoBehaviour
+public class NextLevelScript : MonoBehaviour
 {
+    [SerializeField] string nextLevel;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    // Update is called once per frame
     private void OnTriggerEnter2D()
     {
-        
-            SceneManager.LoadScene("Win");
-       
+
+        SceneManager.LoadScene(nextLevel);
+
     }
-   
- 
-    
 }
