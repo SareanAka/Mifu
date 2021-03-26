db41b812ac5a3bf7cdfed8509c12ec5603fba841
VERMEIT3 <thomas.vermeij2@hva.nl>
2021-03-26 18:29:51 +0100

    Pain Peko

---
+++ Assets/Scripts/Enemies/SpriteRotate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class SpriteRotate : MonoBehaviour
+{
+    public AIPath aiPath;
+    [SerializeField] private SpriteRenderer localSprite;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (aiPath.desiredVelocity.x >= 0.01f)
+        {
+            localSprite.flipX = true;
+        }
+        else if (aiPath.desiredVelocity.x <= -0.01f)
+        {
+            localSprite.flipX = false;
+        }
+    }
+}
